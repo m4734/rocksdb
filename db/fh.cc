231807ff8d179ed6fd1bc3e2b9fe6b4f93b4dc4f
cgmin <m47341@gmail.com>
2020-10-07 17:14:46 +0900

    async hash without test

--- db/fh.cc
+++ db/fh.cc
@@ -1,5 +1,7 @@
 //cgmin fh.cc
 
+#include <unistd.h>
+
 #include "db/fh.h"
 #include "util/hash.h"
 
@@ -17,27 +19,52 @@ namespace ROCKSDB_NAMESPACE {
 			for (j=0;j<bs;j++)
 				fv[i][j] = 0;
 		}
+
+		exit = false;
+		queue_size=1000;
+		key_size=1024;
+		queue_start = 0;
+		queue_end = 0;
+		queue_ready = 0;
+		size_array = new size_t[queue_size];
+		string_array = new char*[queue_size];
+		for (i=0;i<queue_size;i++)
+			string_array[i] = new char[key_size];
+		sleep_time=100;
+
+		t = std::thread(run,&exit,&queue_start,&queue_ready,queue_size,sleep_time,&string_array,&size_array,&fv,fs,bs);
+		printf("fh s\n");
 	}
 
 	FH::~FH()
 	{
+		exit = true;
+
 		int i;
 		for (i=0;i<fs;i++)
 			delete fv[i];
 		delete fv;
+
+		delete size_array;
+		for (i=9;i<queue_size;i++)
+			delete string_array[i];
+		delete string_array;
+
+		t.join();
+		printf("fh f\n");
 	}
 
 	void FH::add(const Slice& key)
 	{
 		int i;
-		uint32_t hv, min=999999999;
+		uint32_t hv;//, min=999999999;
 		for (i=0;i<fs;i++)
 		{
 			hv = hash(i,key);
 //			if (fv[i][hv] < 4294967295)
 				fv[i][hv]++;
-			if (min > fv[i][hv])
-				min = fv[i][hv];
+//			if (min > fv[i][hv])
+//				min = fv[i][hv];
 		}
 //		printf("%d\n",min);
 	}
@@ -47,5 +74,44 @@ namespace ROCKSDB_NAMESPACE {
 //		return Hash(key.data(), key.size(), 0xbc9f1d34+fn)%bs;
 		return Hash64(key.data(), key.size(), 0xbc9f1d34+fn)%bs;
 
+	}
+
+	void FH::enqueue(const Slice& key)
+	{
+		int l;
+		l = queue_end.fetch_add(1) % queue_size;
+		size_array[l]=key.size();
+		memcpy(string_array[l],key.data(),key.size());
+		queue_ready.fetch_add(1);
+	}
+
+	void FH::run(volatile bool* exit_s,int* queue_start_s,std::atomic<int>* queue_ready_s,int queue_size,int sleep_time,char*** string_array_s, size_t** size_array_s,uint32_t*** fv_s,int fs,int bs)
+	{
+		while(!(*exit_s))
+		{
+			if (*queue_start_s < *queue_ready_s)
+			{
+			while (*queue_start_s < *queue_ready_s)
+			{
+		int i,l;
+		uint32_t hv;//, min=999999999;
+		const char* data;
+		size_t size;
+		l = *queue_start_s % queue_size;
+		data = (*string_array_s)[l];
+		size = (*size_array_s)[l];
+		for (i=0;i<fs;i++)
+		{
+			hv = Hash64(data,size,0xbc9f1d34+i)%bs;
+//			if (fv[i][hv] < 4294967295)
+				(*fv_s)[i][hv]++;
+		}
+
+				(*queue_start_s)++;
+			}
+			}
+			else
+			usleep(sleep_time);
+		}
 	}
 }
--- db/fh.h
+++ db/fh.h
@@ -5,6 +5,9 @@
 //#ifndef DB_FH_H_
 //#define DB_FH_H_
 
+#include <atomic>
+#include <thread>
+
 #include "rocksdb/slice.h"
 
 namespace ROCKSDB_NAMESPACE {
@@ -20,7 +23,23 @@ class FH
 		int fs;
 
 		void add(const Slice& key);
+//		void add2(const char* data, size_t size);
 		uint32_t hash(int fn, const Slice& key);
+
+		// async queue
+		int queue_size;
+		std::atomic<int> queue_end;
+		std::atomic<int> queue_ready;
+		int queue_start;
+		size_t* size_array;
+		char** string_array;
+		int key_size;
+		volatile bool exit;
+		int sleep_time;
+		std::thread t;
+
+		void enqueue(const Slice& key);
+		static void run(volatile bool* exit_s, int* queue_start_s, std::atomic<int>* queue_ready_s, int queue_size, int sleep_time, char*** string_array_s, size_t** size_array_s, uint32_t*** fv_s, int fs, int bs);
 
 };
 
